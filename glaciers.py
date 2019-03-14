b48e6ff782835cc17853e00204c0bde5bd831162
Courtney Shafer <courtney@store.apl.washington.edu>
2019-03-14 13:19:29 -0700

    clean up code structure

--- glaciers.py
+++ glaciers.py
@@ -44,21 +44,6 @@ def Create_chips(vrt, date, glacier_info):
     for line in lines:
         glacier_coords = line.split(' ')
         subprocess.call(['gdalwarp', '-t_srs', 'EPSG:3413', '-te', glacier_coords[1], glacier_coords[2], glacier_coords[3], glacier_coords[4], '-tr', '50', '50', vrt, glacier_coords[0]+'.'+date+'.tif'])
-'''
-dated_directories = []
-date_regex = re.compile(r"2018-\d{2}-\d{2}")
-for directory in sorted(os.listdir(mosaics_path)):
-    if date_regex.search(directory) is not None:
-        dated_directories.append(directory)
-
-for date in dated_directories:
-    date_path = mosaics_path + '/' + date
-    os.chdir(date_path)
-    vrt_path = date_path + '/' + Get_vrt(date_path)
-    os.chdir(chips_path)
-    Create_chips(vrt_path, date, glaciers_path)
-'''
-
 
 directories = sorted(os.listdir(mosaics_path))
 date_regex = re.compile(r"2018-\d{2}-\d{2}")  # For example, 2018-04-12
